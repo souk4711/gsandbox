408ae31bab27a8dc9c74957261756f3e069bdb34
John Doe <johndoe@example.com>
2022-06-28 00:02:28 +0800

    Add Executor

---
+++ executor.go
@@ -0,0 +1,44 @@
+package gsandbox
+
+import (
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+type Executor struct {
+	Prog string
+	Args []string
+}
+
+func (e *Executor) Start() error {
+	var cmd = exec.Command(e.Prog, e.Args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWNS |
+			syscall.CLONE_NEWUTS |
+			syscall.CLONE_NEWIPC |
+			syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNET |
+			syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getuid(),
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getgid(),
+				Size:        1,
+			},
+		},
+	}
+
+	return cmd.Run()
+}
--- internal/cmd/run_command.go
+++ internal/cmd/run_command.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+
+	"github.com/souk4711/gsandbox"
 )
 
 func newRunCommand() *cobra.Command {
@@ -9,7 +11,12 @@ func newRunCommand() *cobra.Command {
 		Use:   "run [flags] -- PROGRAM [ARG...]",
 		Short: "Run a program in a sandbox",
 		Args:  cobra.MinimumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var executor = &gsandbox.Executor{
+				Prog: args[0],
+				Args: args[1:],
+			}
+			return executor.Start()
 		},
 	}
 
